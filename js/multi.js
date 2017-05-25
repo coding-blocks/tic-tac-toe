5b75fdf295af31fb2199620f96bcc84c3e0c0927
hereisnaman <naman@outlook.in>
2017-05-25 19:28:41 +0530

    Random and Challenge Mode Added

--- js/multi.js
+++ js/multi.js
@@ -1,116 +1,17 @@
 function multiPlay(row, col) {
   if (checkLegalMove(row, col) == true) {
     if (player == 1) {
-      multiTurn(row, col);
+      playTurn(row, col);
       if (checkWin(1) == true) {
         multiEndGame(1);
       }
-      player = 2;
-    } else {
-      multiTurn(row, col);
+    }
+    else {
+      playTurn(row, col);
       if (checkWin(2) == true) {
         multiEndGame(2);
       }
-      player = 1;
     }
-  }
-}
-
-function multiTurn(row, col) {
-  switch (row) {
-    case 0:
-      switch (col) {
-        case 0:
-          if (player == 1) {
-            $("#square_one_text").html("X");
-            grid[0][0] = 'X';
-          } else {
-            $("#square_one_text").html("O");
-            grid[0][0] = 'O';
-          }
-          break;
-        case 1:
-          if (player == 1) {
-            $("#square_two_text").html("X");
-            grid[0][1] = 'X';
-          } else {
-            $("#square_two_text").html("O");
-            grid[0][1] = 'O';
-          }
-          break;
-        case 2:
-          if (player == 1) {
-            $("#square_three_text").html("X");
-            grid[0][2] = 'X';
-          } else {
-            $("#square_three_text").html("O");
-            grid[0][2] = 'O';
-          }
-          break;
-      }
-      break;
-    case 1:
-      switch (col) {
-        case 0:
-          if (player == 1) {
-            $("#square_four_text").html("X");
-            grid[1][0] = 'X';
-          } else {
-            $("#square_four_text").html("O");
-            grid[1][0] = 'O';
-          }
-          break;
-        case 1:
-          if (player == 1) {
-            $("#square_five_text").html("X");
-            grid[1][1] = 'X';
-          } else {
-            $("#square_five_text").html("O");
-            grid[1][1] = 'O';
-          }
-          break;
-        case 2:
-          if (player == 1) {
-            $("#square_six_text").html("X");
-            grid[1][2] = 'X';
-          } else {
-            $("#square_six_text").html("O");
-            grid[1][2] = 'O';
-          }
-          break;
-      }
-      break;
-    case 2:
-      switch (col) {
-        case 0:
-          if (player == 1) {
-            $("#square_seven_text").html("X");
-            grid[2][0] = 'X';
-          } else {
-            $("#square_seven_text").html("O");
-            grid[2][0] = 'O';
-          }
-          break;
-        case 1:
-          if (player == 1) {
-            $("#square_eight_text").html("X");
-            grid[2][1] = 'X';
-          } else {
-            $("#square_eight_text").html("O");
-            grid[2][1] = 'O';
-          }
-          break;
-        case 2:
-          if (player == 1) {
-            $("#square_nine_text").html("X");
-            grid[2][2] = 'X';
-          } else {
-            $("#square_nine_text").html("O");
-            grid[2][2] = 'O';
-          }
-          break;
-      }
-      break;
   }
 }
 
--- js/random.js
+++ js/random.js
@@ -1,14 +1,9 @@
 function randomPlay(row, col) {
   if (checkLegalMove(row, col) == true) {
-    randomTurn(row, col);
-    console.log('insdfsdfside');
+    playTurn(row, col);
     if (checkWin() == true)
       randomEndGame(1);
     else {
-        if (player == 1)
-            player = 2;
-        else
-            player = 1;
         randomAITurn();
     }
   }
@@ -23,113 +18,11 @@ function randomAITurn () {
       xcol= Math.floor(Math.random()*3);
       if (checkLegalMove(xrow, xcol) == true) {
         flag=0;
-        randomTurn(xrow, xcol);
+        playTurn(xrow, xcol);
         if (checkWin() == true)
             randomEndGame(2);
-        else if (player == 1)
-            player = 2;
-        else
-            player = 1;
       }
     }
-}
-
-function randomTurn(row, col) {
-  switch (row) {
-    case 0:
-      switch (col) {
-        case 0:
-          if (player == 1) {
-            $("#square_one_text").html("X");
-            grid[0][0] = 'X';
-          } else {
-            $("#square_one_text").html("O");
-            grid[0][0] = 'O';
-          }
-          break;
-        case 1:
-          if (player == 1) {
-            $("#square_two_text").html("X");
-            grid[0][1] = 'X';
-          } else {
-            $("#square_two_text").html("O");
-            grid[0][1] = 'O';
-          }
-          break;
-        case 2:
-          if (player == 1) {
-            $("#square_three_text").html("X");
-            grid[0][2] = 'X';
-          } else {
-            $("#square_three_text").html("O");
-            grid[0][2] = 'O';
-          }
-          break;
-      }
-      break;
-    case 1:
-      switch (col) {
-        case 0:
-          if (player == 1) {
-            $("#square_four_text").html("X");
-            grid[1][0] = 'X';
-          } else {
-            $("#square_four_text").html("O");
-            grid[1][0] = 'O';
-          }
-          break;
-        case 1:
-          if (player == 1) {
-            $("#square_five_text").html("X");
-            grid[1][1] = 'X';
-          } else {
-            $("#square_five_text").html("O");
-            grid[1][1] = 'O';
-          }
-          break;
-        case 2:
-          if (player == 1) {
-            $("#square_six_text").html("X");
-            grid[1][2] = 'X';
-          } else {
-            $("#square_six_text").html("O");
-            grid[1][2] = 'O';
-          }
-          break;
-      }
-      break;
-    case 2:
-      switch (col) {
-        case 0:
-          if (player == 1) {
-            $("#square_seven_text").html("X");
-            grid[2][0] = 'X';
-          } else {
-            $("#square_seven_text").html("O");
-            grid[2][0] = 'O';
-          }
-          break;
-        case 1:
-          if (player == 1) {
-            $("#square_eight_text").html("X");
-            grid[2][1] = 'X';
-          } else {
-            $("#square_eight_text").html("O");
-            grid[2][1] = 'O';
-          }
-          break;
-        case 2:
-          if (player == 1) {
-            $("#square_nine_text").html("X");
-            grid[2][2] = 'X';
-          } else {
-            $("#square_nine_text").html("O");
-            grid[2][2] = 'O';
-          }
-          break;
-      }
-      break;
-  }
 }
 
 function randomEndGame(num) {
